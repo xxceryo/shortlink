e52617db73fc49b212df9bc84b068601fcff4f37
xianglong zhou <2764733071@qq.com>
2025-05-18 19:41:26 +0800

    实现第一个接口：通过用户名获取用户信息

--- admin/src/main/java/com/haohaoxuexi/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/haohaoxuexi/shortlink/admin/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.haohaoxuexi.shortlink.admin.controller;
 
+import com.haohaoxuexi.shortlink.admin.dto.resp.UserRespDTO;
+import com.haohaoxuexi.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  * 用户管理控制层
  */
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
     @GetMapping("/api/short-link/v1/user/{username}")
-    public String getUserByUsername(@PathVariable("username") String username) {
-        return "hello " + username;
+    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
+        return userService.getUserByUsername(username);
     }
 }
