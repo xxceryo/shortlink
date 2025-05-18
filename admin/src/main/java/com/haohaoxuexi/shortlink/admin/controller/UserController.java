d41137aa7a226b612140b6987e54f08838cc050b
xianglong zhou <2764733071@qq.com>
2025-05-18 13:50:20 +0800

    创建第一个测试接口

---
+++ admin/src/main/java/com/haohaoxuexi/shortlink/admin/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.haohaoxuexi.shortlink.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ */
+@RestController
+public class UserController {
+    @GetMapping("/api/short-link/v1/user/{username}")
+    public String getUserByUsername(@PathVariable("username") String username) {
+        return "hello " + username;
+    }
+}
